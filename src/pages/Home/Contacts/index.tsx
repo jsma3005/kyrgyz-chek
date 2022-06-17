4d153d0b9d233a1702774718f060972ee522e29c
jsma3005 <megabmx4477@gmail.com>
2022-06-17 11:09:59 +0600

    fixes

--- src/pages/Home/Contacts/index.tsx
+++ src/pages/Home/Contacts/index.tsx
@@ -10,13 +10,13 @@ export const Contacts = () => {
       <div className={cls.container}>
         <div className={cls.contacts}>
           <p>
-            <a href="tel:0771234567">+996 (771) 23-45-67</a>
+            <a href="tel:0771234567">+996 (770) 36-41-97</a>
           </p>
           <p>
-            <a href="mailto:example@gmail.com">example@gmail.com</a>
+            <a href="mailto:example@gmail.com">kyrgyzchek@gmail.com</a>
           </p>
           <p>
-            г. Москва, Газетный переулок, 15
+            р-н. Кара-Суу, с. Кыргыз-Чек
           </p>
 
           <p>​<strong>Мы в соц. сетях:</strong></p>
@@ -31,7 +31,7 @@ export const Contacts = () => {
 
         <div className={cls.map}>
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3032.5445706212436!2d72.79312311545928!3d40.529556429352155!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38bdac05225c2cfd%3A0x16bd460bd75f22ab!2sOsh%20State%20University!5e0!3m2!1sen!2skg!4v1654805187662!5m2!1sen!2skg"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7507.999829530829!2d72.87507502560237!3d40.552804184592794!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38bda8b527f0ca0d%3A0x6ebbdad887998640!2z0JrRi9GA0LPRi9C3LdCn0LXQug!5e0!3m2!1sru!2skg!4v1655442407694!5m2!1sru!2skg"
             width="100%"
             height="400"
             style={{ border: 0 }}
